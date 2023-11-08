ebda655bebd2b70d29ce5980c3738451aa90de85
M1chalS <michalszajner@wp.pl>
2023-11-08 23:07:36 +0100

    Data types

---
+++ main.py
@@ -0,0 +1,20 @@
+#string
+name = "Michał"
+print("Your name is: "+name)
+print(type(name))
+
+#int
+age = 25
+# str = toString()
+print("Your age is: "+str(age))
+print(type(age))
+
+#float
+height = 250.5
+print("Your height is: "+str(height)+"cm")
+print(type(height))
+
+#boolean
+human = True
+print("Are you a human: "+str(human))
+print(type(human))
