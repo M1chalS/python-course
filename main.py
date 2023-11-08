f44ce566ed8036a16be642cc95b8ab8ffdfdcf17
M1chalS <michalszajner@wp.pl>
2023-11-08 23:49:38 +0100

    Multiple assignment, string methods, type casting, input and few math functions

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
-#string
+# string
 name = "Michał"
-print("Your name is: "+name)
+print("Your name is: " + name)
 print(type(name))
 
-#int
+# int
 age = 25
 # str = toString()
-print("Your age is: "+str(age))
+print("Your age is: " + str(age))
 print(type(age))
 
-#float
+# float
 height = 250.5
-print("Your height is: "+str(height)+"cm")
+print("Your height is: " + str(height) + "cm")
 print(type(height))
 
-#boolean
+# boolean
 human = True
-print("Are you a human: "+str(human))
+print("Are you a human: " + str(human))
 print(type(human))
+
+# multiple assignment
+name, age, attractive = "Michał", 19, True
+
+print(name, age, attractive)
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob, Patrick, Sandy, Squidward)
+
+# string methods
+name = "michał"
+
+
+print(len(name))  # length
+print(name.find("i"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())  # is number
+print(name.isalpha())  # is alphabetical letters
+print(name.count("a"))
+print(name.replace("a","o"))
+print(name*4)
+
+# type casting int(), str(), float()
+x = 1
+y = 2.0
+z = "3"
+
+y = int(y)
+z = float(z)
+x = str(x)
+
+print(x)
+print(y)
+print(z*3)
---
+++ math.py
@@ -0,0 +1,15 @@
+import math
+
+pi = math.pi
+x = 1
+y = 2
+z = 3
+
+print(round(pi))
+print(math.ceil(pi))  # round up
+print(math.floor(pi))  # round down
+print(abs(pi)) # absolute value
+print(pow(pi,2))
+print(math.sqrt(pi))
+print(max(x, y, z))
+print(min(x, y, z))
